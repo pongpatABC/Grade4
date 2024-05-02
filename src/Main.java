f3856f6da6f033a3a19e772d8fbfaf2702258b9e
Teacher <pongpatcaroenapsorn01@gmail.com>
2024-05-02 14:44:17 +0700

    grade4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1+1=2");
+        System.out.println("input number");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner . nextInt();
+
+        if (number>=80) {
+            System.out.println("A");
+        }
+        else if (number>=70) {
+            System.out.println("B");
+        }
+        else if (number>=60) {
+            System.out.println("C");
+        }
+        else  {
+            System.out.println("F");
+        }
         }
     }
